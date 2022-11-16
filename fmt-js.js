afda08517167b590ec28da57e12832d7359824d1
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-16 14:23:20 -0500

    change error message to use substring

--- fmt-js.js
+++ fmt-js.js
@@ -65,9 +65,9 @@ function compilefmt (fmtsrc, ohmlang) {
     var fmtcst = internalgrammar.match (fmtsrc);
 
     if (fmtcst.failed ()) {
-        // return [false, "FORMAT: syntax error\n(Use Ohm-Editor to debug format specification (grammar: fmt.ohm))\n\n" + internalgrammar.trace (fmtsrc)];
 	console.error (internalgrammar);
-        return [false, "FORMAT: syntax error\n(Use Ohm-Editor to debug format specification) rightmostPosition=" + fmtcst.getRightmostFailurePosition() + '\n' + fmtsrc];
+	let len = fmtcst.getRightmostFailurePosition();
+        return [false, "FORMAT: syntax error in fabrication specification\nrightmostPosition=" + len + '\n' + fmtsrc.substring (0, len)];
     }
     // Step 1b. Transpile User's FMT spec to a JS object (for use with Ohm-JS)
     try {
