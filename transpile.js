47f106a015e792e36f8bf64757b7e53a9ac1d089
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-20 14:44:32 -0500

    fab spec === ''

--- transpile.js
+++ transpile.js
@@ -11,7 +11,7 @@ function transpile (src, grammarName, grammars, fmt, ohmlang, compfmt, supportfn
     [matchsuccess, trgrammar, cst, errormessage] = patternmatch (src, grammarName, grammars, ohmlang);
     if (!matchsuccess) {
 	return [false, "", "pattern matching error<br><br>" + errormessage];
-    } else if (fmt === undefined) {
+    } else if (fmt === undefined || fmt === '') {
 	return [false, "", "pattern matching succeeded (but without fabrication)<br><br>" + errormessage];
     } else {
 	[success, semanticsFunctionsAsString] = compfmt (fmt, ohmlang);
