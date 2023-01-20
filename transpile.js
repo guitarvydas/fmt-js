61be3cd65c840c5ccf427461aa532f2d287f5e95
Paul Tarvydas <guitarvydas@gmail.com>
2023-01-20 07:58:13 -0500

    remove grammarName

--- transpile.js
+++ transpile.js
@@ -1,14 +1,6 @@
-// return 3 item from transpile
-function vtranspile (v) {
-    // v is { tracing: boolean, traceDepth: int, src: String, grammarName: String, grammars: String, fmt : String, ohm: function, compilefmt: function}
-    tracing = v.tracing;
-    traceDepth = v.traceDepth;
-    return transpile (v.src, v.grammarName, v.grammars, v.fmt, v.ohm, v.compilefmt);
-}
-
-function transpile (src, grammarName, grammars, fmt, ohmlang, compfmt, supportfname) {
+function transpile (src, grammar, fmt, ohmlang, compfmt, supportfname) {
     
-    [matchsuccess, trgrammar, cst, errormessage] = patternmatch (src, grammarName, grammars, ohmlang);
+    [matchsuccess, trgrammar, cst, errormessage] = patternmatch (src, grammar, ohmlang);
     if (!matchsuccess) {
 	return [false, "", "pattern matching error<br><br>" + errormessage];
     } else if (fmt === undefined || fmt === '') {
